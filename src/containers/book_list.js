6be4992a3e752fd8f622d1cbc5d269f9dfb6626f
Zach Bird <zachthebird@gmail.com>
2017-07-27 15:44:51 -0500

    updating appOD

---
+++ src/containers/book_list.js
@@ -0,0 +1,43 @@
+import  React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+    renderList() {
+        return  this.props.books.map((book) => {
+            return (
+                <li
+                    key={book.title}
+                    className='list-group-item'
+                    onClick={() => this.props.selectBook(book)}>
+                    {book.title}
+                </li>
+            );    
+        });
+    }
+    render(){
+        return(
+            <ul className='list-group col-sm-4'>
+                {this.renderList()}
+            </ul>
+        )
+    }     
+}
+
+function mapStateToProps(state) {
+    //Whatever is returned from here will show up as
+    //props inside of BookList
+    return {
+        books: state.books
+    };
+}
+//anything returned from this function will end up as props on
+//the BookList container
+function mapDispatchToProps(dispatch){
+    //whenever selectBook is called the result should be passed
+    //to all of our reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BookList); 
+
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return [
+        { title: `Javascript: the Good Part`, pages: 322 },
+        { title: `Harry Potter and The Sorceror's stone`, pages: 569 },
+        { title: `JSX for Dummies`, pages: 210},
+        { title: `Eloquent Ruby`, pages: 55 },
+        { title: `The Jungle Book`, pages: 30 }
+    ]
+}
+
